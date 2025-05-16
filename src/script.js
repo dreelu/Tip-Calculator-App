cd43482c207e68f18e02b80a3ecfc470017b3945
eb <andre.luis8173@gmail.com>
2025-05-16 13:42:04 -0300

    0.1

---
+++ src/script.js
@@ -0,0 +1,23 @@
+let TipPercantage = null
+
+function TipValue(n) {
+    TipPercantage = n/100
+}
+
+function CalculateTip() {
+    let BillInput = document.querySelector("input#bill")
+    let PeopleAmount = document.querySelector("input#npeople")
+
+    let BillValue = BillInput.value
+    let PoeopleValue = PeopleAmount.value
+
+    let TipAmount = (BillValue / PoeopleValue) * TipPercantage
+    let Total = BillValue / PoeopleValue + TipAmount
+
+    window.alert(`${TipPercantage}`) // Debug Test
+}
+
+   // Matemática:
+    //  Tip Amount = (Bill * 15%) / 5
+    // Total: Bill / 5 + Tip Amount
+
